b65a6f81f0aef91bc0a8671ad2fffb96fdd35044
thaissouzaa <thaisdesouzaa24@gmail.com>
2025-06-05 17:13:45 -0300

    botoes

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
+
+//POPUP
+
 const abrir_botao = document.querySelectorAll(".abrir-popup")
 const fechar_botao = document.getElementById("fechar-popup")
 const popup = document.getElementById("popup")
 
 
-abrir_botao.forEach(botao => {
-    botao.addEventListener("click",function(){
-    popup.style.display = "block"
-    })
-});
+if (popup && fechar_botao && abrir_botao.length > 0) {
+    abrir_botao.forEach(botao => {
+        botao.addEventListener("click", function () {
+            popup.style.display = "block";
+        });
+    });
 
 fechar_botao.addEventListener("click", function(){
     popup.style.display = "none";
@@ -18,8 +22,13 @@ window.addEventListener("click", function(evento){
         popup.style.display = "none";
     }
 });
+}
 
-const botao_screen = getElementById("botao_screen");
+//Botão para ir para cima na página
+
+const botao_screen = document.getElementById("botao_screen");
+
+if (botao_screen) {
 
 window.onscroll = function(){
     if (document.documentElement.scrollTop > 100){
@@ -30,9 +39,39 @@ window.onscroll = function(){
     }
 };
 
-    window.addEventListener("click", function() {
+    botao_screen.addEventListener("click", function() {
         window.scrollTo({
             top:0,
             behavior:"smooth"
         });
     });
+}
+
+    //Filtro para as fotos galerias
+
+    
+const botoes = document.querySelectorAll(".botoes button")
+const fotos = document.querySelectorAll(".foto")
+
+if (botoes && fotos.length > 0) {
+
+botoes.forEach(botao=>{
+    botao.addEventListener("click",()=>{
+        const categoria = botao.getAttribute("data-categoria");
+
+        fotos.forEach(foto =>{
+            if(categoria=="tudo"){
+                foto.style.display = "inline-block";
+            }
+            else{
+                if (foto.getAttribute("data-categoria") === categoria){
+                    foto.style.display = "inline-block"
+                }
+                else{
+                foto.style.display = "none"
+            }
+            }
+        });
+    });
+});
+}
